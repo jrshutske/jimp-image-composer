197afee05c5b8002417bd84aa7c85666a32e3b44
jrshutske <jackshutske@gmail.com>
2020-03-05 00:52:45 -0600

    finish image layers

--- mapleImage.js
+++ mapleImage.js
@@ -2,39 +2,56 @@ const Jimp = require('jimp');
 const fetch = require('node-fetch');
 const download = require('image-downloader')
 
-const getInfo = () => {
-  fetch(`https://maplelegends.com/api/getavatar?name=lean`).then(function(results) {
-  return results;
-}).then(function(results) {
-  downloadIMG(results.url)
-}).catch(function(err) {
-  console.log("There was an error finding that." + err);
-});
+const getInfo = async (characterName) => {
+  try {
+    let response = await fetch(`https://maplelegends.com/api/character?name=${characterName}`)
+    let charaterData = await response.json()
+    let characterDest = await downloadImage(`https://maplelegends.com/api/getavatar?name=${characterName}`)
+    createImage(characterDest, charaterData)
+  } catch(e) {
+    console.error(e)
+  }
 }
 
-async function downloadIMG(url) {
+async function downloadImage(url) {
+  let dest = './tmp/characterImage.png';
   try {
-    const { filename, image } = await download.image({
-      url: url,
-      dest: './tmp/thing.png'                  
-    })
-    console.log(url) // => /path/to/dest/image.jpg 
+    await download.image({url, dest})
+    return dest;
   } catch (e) {
     console.error(e)
   }
 }
- 
-Jimp.read('./tmp/character.png').then(image1 => {
-  Jimp.read('./tmp/leafre.jpg').then(image2 => {
-    image1 
-    .resize(200, 314)  
-    image2
-      .composite( image1, 500, 240),
-    Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(font => {
-      image2.print(font, 10, 10, 'Hello world!')
-        .write('new-character.png')
-    });
-  }).catch(err => console.error(err));
-}).catch(err => console.error(err));
 
-getInfo();
+async function createImage(characterDest, charaterData) {
+  try {
+    let characterImage = await Jimp.read(characterDest);
+    let backgroundImage = await Jimp.read('./tmp/leafre.jpg');
+    let greyRect = await Jimp.read('./tmp/greyRect.png');
+    let font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE)
+    characterImage.resize(200, 200)
+    backgroundImage.crop(0, 200, 600, 400)
+    greyRect.resize(290, 380)
+    greyRect.brightness(-.8)
+    greyRect.opacity(.8)
+    greyRect.print(font, 20, 30, charaterData.name)
+    greyRect.print(font, 20, 70, `Level: ${charaterData.level}`)
+    greyRect.print(font, 20, 110, `Exp: ${charaterData.level === 200 ? "N/A" : charaterData.exp}`)
+    greyRect.print(font, 20, 150, `Fame: ${charaterData.fame}`)
+    greyRect.print(font, 20, 190, `Cards: ${charaterData.cards}`)
+    greyRect.print(font, 20, 230, `Quests: ${charaterData.quests}`)
+    greyRect.print(font, 20, 270, `Job: ${charaterData.job}`)
+    greyRect.print(font, 20, 310, `Guild: ${charaterData.guild}`)
+    backgroundImage.composite(greyRect, 10, 10)
+    backgroundImage.composite(characterImage, 350, 160)
+    backgroundImage.write('./tmp/newCharacter.png')
+  } catch(e){
+    console.log(e)
+  }
+}
+
+const myArgs = process.argv.slice(2);
+
+!myArgs[0] 
+  ? console.info("\x1b[35m", "Please provide a character name.", '\x1b[0m') 
+  : getInfo(myArgs[0]);
